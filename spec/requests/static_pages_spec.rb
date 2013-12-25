8e827e27651778fe87d6bbb8fad335156bb8ea08
LTZorg <ltzorg@gmail.com>
2013-12-26 00:10:09 +0400

    Заполнение шаблонов

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	subject {page}
+
 	  let(:base_title) { "Заявки на перевозки" }
 
 	  describe "Home page" do
+	  	before {visit root_path}
 
-	    it "должна содержать контент 'Добро пожаловать'" do
-	      visit '/static_pages/home'
-	      expect(page).to have_content("Добро пожаловать")
-	    end
-
-		it "должна содержать заголовок 'Заявки на перевозки'" do
-	      visit '/static_pages/home'
-	      expect(page).to have_title("#{base_title}")
-	    end
-
-		it "не должна содержать заголовок 'Заявки на перевозки |'" do
-	      visit '/static_pages/home'
-	      expect(page).not_to have_title("Заявки на перевозки |")
-	    end
+	    it { should have_content('Добро пожаловать') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('Заявки на перевозки |') }
 
 	  end
 
 
 	  describe "Help page" do
+	  	before {visit help_path}
 
-	    it "должна содержать контент 'Справка'" do
-	      visit '/static_pages/help'
-	      expect(page).to have_content('Справка')
-	    end
-	    
-	    it "должна содержать заголовок 'Заявки на перевозки | Справка'" do
-	      visit '/static_pages/help'
-	      expect(page).to have_title("#{base_title} | Справка")
-	    end
+	    it { should have_content('Справка') }
+	    it { should have_title(full_title('Справка')) }
 
 	  end
 
 	  describe "Contact page" do
+	  	before {visit contact_path}
 
-	    it "должна содержать контент 'Контакты'" do
-	      visit '/static_pages/contact'
-	      expect(page).to have_content('Контакты')
-	    end
-	    
-	    it "должна содержать заголовок 'Заявки на перевозки | Контакты'" do
-	      visit '/static_pages/contact'
-	      expect(page).to have_title("#{base_title} | Контакты")
-	    end
+	    it { should have_content('Контакты') }
+	    it { should have_title(full_title('Контакты')) }
 
 	  end
 
